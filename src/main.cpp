d71681d8146741cd6edde11c606c7b623d0b16fc
bo-wu <wubo.gfkd@gmail.com>
2015-01-16 00:59:36 +0800

    initial the implementation

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp  Version:  1.0  Created:  01/16/2015 12:51:08 AM
+ *
+ *    Description:  main function
+ *
+ *         Author:  Bo Wu (Robert), wubo.gfkd@gmail.com
+ *	    Copyright:  Copyright (c) 2015, Bo Wu
+ *   Organization:  National University of Defense Technology
+ *
+ * =====================================================================================
+ */
+
+int main(int argc, char** argv)
+{
+
+	return 0;
+}
